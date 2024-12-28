24a617cd699d0fb733b4b685db930ca286a545a5
thinkmonkeyconsultoria <thinkmonkeyconsultoria@gmail.com>
2024-12-28 17:15:57 +0000

    Updating app.py

--- views/movimentacao.py
+++ views/movimentacao.py
@@ -5,14 +5,30 @@ st.set_page_config(page_title="Controle de Movimentação", page_icon="💰", la
 
 st.title("Controle de Movimentação")
 
-file_path = "bases/Planilha de Movimentação.xlsx"
+@st.cache_data
+def carregar_dados():
+  file_path = "bases/Planilha de Movimentação.xlsx"
 
-fundos_df = pd.read_excel(file_path, sheet_name="Fundos")
-acoes_df = pd.read_excel(file_path, sheet_name="Ações")
-renda_fixa_df = pd.read_excel(file_path, sheet_name="Renda Fixa")
+  fundos_df = pd.read_excel(file_path, sheet_name="Fundos")
+  acoes_df = pd.read_excel(file_path, sheet_name="Ações")
+  renda_fixa_df = pd.read_excel(file_path, sheet_name="Renda Fixa")
+
+  dados_dic = {
+      "Fundos":fundos_df,
+      "Ações":acoes_df,
+      "Renda Fixa":renda_fixa_df
+  }
+
+  return dados_dic
+
+bases = carregar_dados()
 
 selecionar_ativo = st.pills(
     "Selecione o Ativo",
     options=["Fundos","Ações","Renda Fixa"],
     selection_mode="single",
 )
+
+base_selecionada = bases[selecionar_ativo]
+
+st.dataframe(base_selecionada)
