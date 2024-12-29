71fe2793786b658cebc347b960eeea0330c6c2ed
thinkmonkeyconsultoria <thinkmonkeyconsultoria@gmail.com>
2024-12-29 19:53:37 +0000

    Updating app.py

--- views/movimentacao.py
+++ views/movimentacao.py
@@ -2,15 +2,16 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
 
-
 st.set_page_config(page_title="Controle de Movimentação", page_icon="💰", layout="wide")
 
-header_1, header_2 = st.columns(2)
+header_1, header_2 = st.columns([1,3])
 
 with header_1:
+
   st.image("Imagens/logo.png")
 
 with header_2:
+
   st.title("Controle de Movimentação")
 
 @st.cache_data
---
+++ views/revisao.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+# Streamlit Page Config
+# st.set_page_config(page_title="Controle de Movimentação", page_icon="💰", layout="wide")
+
+# Carregar excel
+# pd.read_excel(file_path, sheet_name="Fundos")
+
+# Cache Decorator
+# @st.cache_data
+
+# Mostrar dataframe
+# st.dataframe(filtered_df,hide_index=True,use_container_width=True)
+
+# Pills
+# st.pills("Selecione o Ativo",options=["Fundos","Ações","Renda Fixa"],selection_mode="single",default="Fundos")
