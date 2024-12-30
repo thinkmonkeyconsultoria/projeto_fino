c6b6dd9d61285574427fec719f98c05097ab9888
thinkmonkeyconsultoria <thinkmonkeyconsultoria@gmail.com>
2024-12-30 19:15:48 +0000

    Updating app.py

--- views/revisao.py
+++ views/revisao.py
@@ -4,12 +4,13 @@ import pandas as pd
 # Streamlit Page Config
 st.set_page_config(page_title="Controle de Movimentação", page_icon="💰", layout="wide")
 
+seletor_de_abas = st.pills("Selecione o Ativo",options=["Fundos","Ações","Renda Fixa"],selection_mode="single",default="Fundos")
+
 # Carregar excel
 file_path = "bases/Planilha de Movimentação.xlsx"
-fundos_df = pd.read_excel(file_path, sheet_name="Fundos")
+fundos_df = pd.read_excel(file_path, sheet_name=seletor_de_abas)
 
 # Pills
-seletor_de_abas = st.pills("Selecione o Ativo",options=["Fundos","Ações","Renda Fixa"],selection_mode="single",default="Fundos")
 
 st.dataframe(fundos_df,hide_index=True,use_container_width=True)
 
