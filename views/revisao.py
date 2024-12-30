c43c19c80dbc953a8e955b609d9563ebbeccdc03
thinkmonkeyconsultoria <thinkmonkeyconsultoria@gmail.com>
2024-12-30 19:11:46 +0000

    Updating app.py

--- views/revisao.py
+++ views/revisao.py
@@ -2,16 +2,19 @@ import streamlit as st
 import pandas as pd
 
 # Streamlit Page Config
-# st.set_page_config(page_title="Controle de Movimentação", page_icon="💰", layout="wide")
+st.set_page_config(page_title="Controle de Movimentação", page_icon="💰", layout="wide")
 
 # Carregar excel
-# pd.read_excel(file_path, sheet_name="Fundos")
+file_path = "bases/Planilha de Movimentação.xlsx"
+fundos_df = pd.read_excel(file_path, sheet_name="Fundos")
+
+st.dataframe(fundos_df,hide_index=True,use_container_width=True)
 
 # Cache Decorator
 # @st.cache_data
 
 # Mostrar dataframe
-# st.dataframe(filtered_df,hide_index=True,use_container_width=True)
+
 
 # Pills
 # st.pills("Selecione o Ativo",options=["Fundos","Ações","Renda Fixa"],selection_mode="single",default="Fundos")
